be0663093dad0c5342a3f0f0eca25f2a67d2df6c
Vladislav <mypulse@yandex.ru>
2022-10-27 17:07:09 +0300

    решение 2-й задачи

---
+++ Lessons01/Examples/Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 79;
+int b = 67;
+int max;
+int min;
+
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+
+Console.WriteLine("Максимальное значение " + max);
+Console.WriteLine("Минимальное значение " + min);
