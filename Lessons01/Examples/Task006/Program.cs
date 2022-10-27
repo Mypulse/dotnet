bb0e3293bab88be529beb79a1718a4e87739dfd2
Vladislav <mypulse@yandex.ru>
2022-10-28 00:21:21 +0300

    Решение задачи № 2

---
+++ Lessons01/Examples/Task006/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 17;
+string Even = "четное";
+string NotEven = "не четное";
+
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Это число " + Even);
+}
+else
+{
+    Console.WriteLine("Это число " + NotEven);
+}
