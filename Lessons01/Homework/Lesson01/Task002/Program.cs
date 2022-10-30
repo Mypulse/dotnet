b140f4d99a713e144cb3b35b083d393ce2f9fba4
Vladislav <mypulse@yandex.ru>
2022-10-30 22:44:11 +0300

    решение задачи

---
+++ Lessons01/Homework/Lesson01/Task002/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 55;  
+int b = 11;
+int c = 99;
+int max = 0;
+
+if (a > b)
+{
+    if(a > c)
+    
+        max = a;
+        else
+        max = c;
+    
+}
+else
+{
+    if(c > b)
+    
+        max = c;
+        else
+        max = b;
+    
+}
+Console.WriteLine(max);
