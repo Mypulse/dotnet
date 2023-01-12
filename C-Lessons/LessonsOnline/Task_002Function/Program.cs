4f51e42945d621860cc851ab14c46394f7a29613
Vladislav <mypulse@yandex.ru>
2023-01-12 14:00:17 +0300

    new commit

---
+++ C-Lessons/LessonsOnline/Task_002Function/Program.cs
@@ -0,0 +1,10 @@
+﻿//Работа с функцими
+
+int GetSquare(int number) // объявлять функции лучше сверху 
+{
+    return number * number;
+}
+
+int number = 7;
+int square = GetSquare(number);
+Console.WriteLine(square);
