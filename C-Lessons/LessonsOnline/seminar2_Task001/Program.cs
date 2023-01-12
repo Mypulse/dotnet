39cc80c04509aebcd7af7ccedd51e05070111657
Vladislav <mypulse@yandex.ru>
2023-01-13 01:10:50 +0300

    lessons 2

---
+++ C-Lessons/LessonsOnline/seminar2_Task001/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+Console.WriteLine("Эта программа принимает на вход трехзначное число и на выходе показывает послднюю цифру");
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+int get_last_digit(int number)
+{
+    return number % 10;
+}
+
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не корректное число!");
+}
+else
+{
+    Console.WriteLine($"Последняя цифра числа {number} - {get_last_digit(number)}");
+}
+
+
+
---
+++ C-Lessons/LessonsOnline/seminar2_Task002/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Написать программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа.
+78 -> 8
+12 -> 2
+85 -> 8
+*/
+
+int GetMaxDigit(int num)
+{
+    int maxDigit;
+    int second_decimal_place = num % 10;
+    int first_decimal_place = num / 10;
+
+    if(second_decimal_place >= first_decimal_place)
+    {
+        maxDigit = second_decimal_place;
+    }
+    else
+    {
+        maxDigit = first_decimal_place;
+    }
+    return maxDigit;
+}
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка [10, 99] {number}");
+
+int result = GetMaxDigit(number);
+System.Console.WriteLine($"Максимальное цифра {number} -> {result}");
